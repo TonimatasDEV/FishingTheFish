0c44187db13ef2eb1bf0abf9a7262690a6b4f01e
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2026-08-17 16:35:30 +0200

    feat: Create fishing speed and hooking speed attributes

---
+++ common/src/main/java/dev/tonimatas/ftf/registry/ModAttributes.java
@@ -0,0 +1,26 @@
+package dev.tonimatas.ftf.registry;
+
+import dev.architectury.registry.level.entity.EntityAttributeRegistry;
+import dev.architectury.registry.registries.DeferredRegister;
+import dev.architectury.registry.registries.RegistrySupplier;
+import dev.tonimatas.ftf.FishingTheFish;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.entity.EntityTypes;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.RangedAttribute;
+import net.minecraft.world.entity.player.Player;
+
+public class ModAttributes {
+    private static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(FishingTheFish.MOD_ID, Registries.ATTRIBUTE);
+
+    public static final RegistrySupplier<Attribute> FISHING_SPEED = ATTRIBUTES.register("fishing_speed", () -> new RangedAttribute(
+       "attributes.fishingthefish.fishing_speed", 0, 0, 600).setSyncable(true));
+
+    public static final RegistrySupplier<Attribute> HOOKING_SPEED = ATTRIBUTES.register("hooking_speed", () -> new RangedAttribute(
+            "attributes.fishingthefish.hooking_speed", 0, 0, 80).setSyncable(true));
+
+    public static void register() {
+        ATTRIBUTES.register();
+        EntityAttributeRegistry.register(() ->  EntityTypes.PLAYER, () -> Player.createAttributes().add(FISHING_SPEED.asHolder(), 0).add(HOOKING_SPEED.asHolder(), 0));
+    }
+}
