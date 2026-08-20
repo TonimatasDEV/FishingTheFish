2f99845595cc64ed18dcf30afa4f2b6d30acbaad
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2026-08-20 13:19:46 +0200

    feat: New attributes

--- common/src/main/java/dev/tonimatas/ftf/registry/ModAttributes.java
+++ common/src/main/java/dev/tonimatas/ftf/registry/ModAttributes.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.EntityTypes;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.RangedAttribute;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
 
 public class ModAttributes {
     private static final DeferredRegister<Attribute> ATTRIBUTES = DeferredRegister.create(FishingTheFish.MOD_ID, Registries.ATTRIBUTE);
@@ -18,6 +19,15 @@ public class ModAttributes {
 
     public static final RegistrySupplier<Attribute> HOOKING_SPEED = ATTRIBUTES.register("hooking_speed", () -> new RangedAttribute(
             "attributes.fishingthefish.hooking_speed", 0, 0, 80).setSyncable(true));
+
+    public static final RegistrySupplier<Attribute> CREATURE_CHANCE = ATTRIBUTES.register("creature_chance", () -> new RangedAttribute(
+            "attributes.fishingthefish.creature_chance", 0, 0, 100).setSyncable(true));
+
+    public static final RegistrySupplier<Attribute> LOOT_CHANCE = ATTRIBUTES.register("loot_chance", () -> new RangedAttribute(
+            "attributes.fishingthefish.loot_chance", 0, 0, 100).setSyncable(true));
+
+    public static final RegistrySupplier<Attribute> DOUBLE_HOOK_CHANCE = ATTRIBUTES.register("double_hook_chance", () -> new RangedAttribute(
+            "attributes.fishingthefish.double_hook_chance", 0, 0, 100).setSyncable(true));
 
     public static void register() {
         ATTRIBUTES.register();
