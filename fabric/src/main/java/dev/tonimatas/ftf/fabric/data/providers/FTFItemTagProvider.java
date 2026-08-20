cbcb905e8680745b405e7f91bf9e078e034a8dfa
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2026-08-20 13:20:14 +0200

    feat: Add new tags

--- fabric/src/main/java/dev/tonimatas/ftf/fabric/data/providers/FTFItemTagProvider.java
+++ fabric/src/main/java/dev/tonimatas/ftf/fabric/data/providers/FTFItemTagProvider.java
@@ -4,8 +4,10 @@ import dev.tonimatas.ftf.registry.ModItems;
 import dev.tonimatas.ftf.registry.ModTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.references.ItemIds;
+import net.minecraft.tags.ItemTags;
 import org.jspecify.annotations.NonNull;
 
 import java.util.concurrent.CompletableFuture;
@@ -20,5 +22,9 @@ public class FTFItemTagProvider extends FabricTagsProvider.ItemTagsProvider {
         tag(ModTags.Items.FISHING_RODS)
                 .add(ModItems.EXAMPLE.getKey())
                 .add(ItemIds.FISHING_ROD);
+        tag(ConventionalItemTags.FISHING_ROD_TOOLS)
+                .add(ModItems.EXAMPLE.getKey());
+        tag(ItemTags.FISHING_ENCHANTABLE)
+                .add(ModItems.EXAMPLE.getKey());
     }
 }
